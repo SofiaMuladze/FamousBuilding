980f816f859290b74af5fece25f33f95a676da35
Sofia <So.muladze@gmail.com>
2021-08-14 02:11:11 +0200

    Initial Commit

---
+++ FamousBuilding/FamousBuildingApp.swift
@@ -0,0 +1,17 @@
+//
+//  FamousBuildingApp.swift
+//  FamousBuilding
+//
+//  Created by Sofia Muladze on 14/08/21.
+//
+
+import SwiftUI
+
+@main
+struct FamousBuildingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
